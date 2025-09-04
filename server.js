fed198cbc38e118f8112d221226e0399d2549ea6
shudhanvi <shudhanvi@gmail.com>
2025-09-04 18:03:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "https://shudh-anvi-main.onrender.com",
+  "https://shudh.anvirobotics.com",
 ];
 
 const corsOptions = {
